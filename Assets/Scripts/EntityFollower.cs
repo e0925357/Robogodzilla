08d4de655104399e869560295b1a63b0cbcc9224
Stefan Zaufl <stefan.zaufl@hotmail.com>
2015-01-23 23:27:00 +0100

    Added camera control

---
+++ Assets/Scripts/EntityFollower.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EntityFollower : MonoBehaviour {
+	public float followingSpeed = 1f;
+	public float sqrDeadzone = 0.01f;
+	public Transform targetEntity;
+	
+	private Vector3 startTransOffset;
+
+	// Use this for initialization
+	void Start () {
+		startTransOffset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 diffVec = targetEntity.position - transform.position + startTransOffset;
+		
+		if(diffVec.sqrMagnitude > sqrDeadzone) {
+			transform.position = transform.position + diffVec.normalized * followingSpeed * Time.deltaTime;
+		}
+	}
+}
---
+++ Assets/Scripts/RandomTeleport.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomTeleport : MonoBehaviour {
+	private float time = 0f;
+	
+	public float intervall = 10f;
+	public float teleportRadius = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		
+		if(time > intervall) {
+			transform.position += new Vector3(Random.value*teleportRadius, 0, Random.value*teleportRadius);
+			time = 0;
+		}
+	}
+}
