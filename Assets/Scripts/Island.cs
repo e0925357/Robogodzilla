9a4df7c09467c1711e82da2e17449e8aad77077c
David Portisch <david.portisch@outlook.com>
2015-01-24 00:29:10 +0100

    Added island prefab and script stub

---
+++ Assets/Scripts/Island.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Island : MonoBehaviour {
+
+	public AnimationCurve spawnCurve;
+
+	public AnimationCurve dieCurve;
+
+	public float spawnTime = 1;
+
+	public float spawnHeight = -1;
+
+	private bool islandSpawned = false;
+	private float spawnTimer = 0;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!islandSpawned) {
+			spawnTimer += Time.deltaTime;
+		}
+	}
+}
