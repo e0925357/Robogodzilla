53aa82a740ac259680b77a11b86956a7141858d7
Stefan Zaufl <stefan.zaufl@hotmail.com>
2015-01-24 00:18:41 +0100

    Added Player, IslandManager stubs

---
+++ Assets/Scripts/IslandManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IslandManager : MonoBehaviour {
+	public Player player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void islandClicked() {
+		
+	}
+}
