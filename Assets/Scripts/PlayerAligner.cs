091d1afb3be6a67c52b369430f1ed695c56a4195
David Portisch <david.portisch@outlook.com>
2015-01-24 17:27:26 +0100

    Moved the turn logic of from the Player script to the PlayerAligner

---
+++ Assets/Scripts/PlayerAligner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAligner : MonoBehaviour {
+
+	public Camera playerCamera;
+	public Player player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!player.IsJumping) {
+			Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+			Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+			
+			RaycastHit hit;
+			Physics.Raycast(ray, out hit);
+			Vector3 point = hit.point;
+			point.y = 0;
+			
+			Vector3 direction = point - transform.position;
+			direction.Normalize();
+			
+			Vector3 rotation = transform.localRotation.eulerAngles;
+
+			float dotProduct = Vector3.Dot(Vector3.right, direction);
+			float angle = Mathf.Acos(dotProduct);
+			
+			if(direction.z > 0){
+				angle *= -1;
+			}
+
+			rotation.y = angle * 180.0f / Mathf.PI;
+
+			transform.rotation = Quaternion.identity;
+			transform.Rotate(rotation);
+		}
+	}
+}
