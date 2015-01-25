0b39f9c4671a441a1ca32ccca38137051f3b2f0b
Stefan Zaufl <stefan.zaufl@hotmail.com>
2015-01-25 13:02:57 +0100

    Added main menu.

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public Image creditsPanel;
+	public Image helpPanel;
+
+	public void quit() {
+		Application.Quit();
+	}
+	
+	public void credits() {
+		bool newValue = !creditsPanel.enabled;
+		
+		creditsPanel.enabled = newValue;
+		
+		Text[] texts = creditsPanel.GetComponentsInChildren<Text>();
+		
+		foreach(Text text in texts) {
+			text.enabled = newValue;
+		}
+	}
+	
+	public void play() {
+		Application.LoadLevel(1);
+	}
+}
