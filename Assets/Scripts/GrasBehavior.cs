e24270828b9017a79c2f42453981d797148a4e4b
David Portisch <david.portisch@outlook.com>
2015-01-25 12:56:32 +0100

    Sheeps are now flying desperately around

---
+++ Assets/Scripts/GrasBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrasBehavior : SpecialIslandBehavior {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public override void OnPlayerLanded ()
+	{
+		Sheep[] sheeps = GetComponentsInChildren<Sheep> ();
+
+		foreach (Sheep sheep in sheeps) {
+			sheep.launchSheep();
+		}
+	}
+}
---
+++ Assets/Scripts/Sheep.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sheep : MonoBehaviour {
+
+	public AnimationCurve heightCurve;
+	public float lifetime;
+
+	private Vector3 movement;
+	private bool launched = false;
+
+	private float timer = 0;
+	private float startY;
+	private Vector3 rotation;
+	private float launchDelay;
+	private bool launchDelayActive = true;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (launched && timer <= lifetime) {
+			timer += Time.deltaTime;
+		
+			if(launchDelayActive) {
+				if(timer >= launchDelay) {
+					launchDelayActive = false;
+					timer = 0;
+
+					audio.pitch = 0.8f + Random.value * 0.8f;
+					audio.PlayDelayed(Random.value * 0.4f);
+				}
+			}
+			else {
+				if(timer >= lifetime) {
+					Destroy(this.gameObject);
+				}
+
+				Vector3 newPosition = transform.position;
+				newPosition += movement * Time.deltaTime;
+				newPosition.y = startY + heightCurve.Evaluate(timer / lifetime) * 4;
+
+				transform.position = newPosition;
+				transform.Rotate(rotation * Time.deltaTime);
+			}
+		}
+	}
+
+	public void launchSheep(){
+		Vector3 projectedPosition = transform.position;
+		projectedPosition.y = 0;
+
+		Vector3 projectedOrigin = transform.parent.position;
+		projectedOrigin.y = 0;
+
+		movement = projectedPosition - projectedOrigin;
+		movement.Normalize ();
+
+		movement *= 6 + Random.value  * 2;
+
+		startY = transform.position.y;
+		rotation.x = 180 + 360 * Random.value;
+		rotation.y = 180 + 360 * Random.value;
+		rotation.z = 180 + 360 * Random.value;
+
+		launchDelay = Random.value * 0.1f;
+
+		launched = true;
+	}
+}
